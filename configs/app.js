a3149143dd5670de398defebc4bcef49315e876e
MaferChoc <mariasolorzano@sanjudastadeo.edu.gt>
2026-07-08 15:24:37 -0600

    mensaje para guardar el commit

--- configs/app.js
+++ configs/app.js
@@ -31,13 +31,15 @@ const middlewares = (app) => {
     }));
 
     // Manejar explícitamente las peticiones OPTIONS
-    app.options('*', (req, res) => {
-        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.header('Access-Control-Allow-Credentials', 'true');
-        res.sendStatus(204);
-    });
+ /*
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(204);
+});
+*/
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
